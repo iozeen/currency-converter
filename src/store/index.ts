755a79c56ec19e6b778a98cb3477ffd76c68bcf7
Ilya Zinkevych <ilya.zinkevych@gmail.com>
2020-04-20 00:14:00 +0300

    store logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
+const middleware = [sagaMiddleware, logger];
 
 const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(...middleware)
+  applyMiddleware(...middleware),
 ));
 
 sagaMiddleware.run(rootSaga);
