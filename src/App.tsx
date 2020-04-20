2017992b57cd3bfdb7372d4f4725697873a58cd0
Ilya Zinkevych <ilya.zinkevych@gmail.com>
2020-04-20 17:18:47 +0300

    App styling, CurrencyConverter page

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-
 import { useDispatch } from 'react-redux';
+
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 import CurrencyConverter from './pages/CurrencyConverter';
 import Currencies from './pages/Currencies';
 import AppBar from './components/AppBar';
 import { getCurrencies } from './actions/currencies';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  container: {
+    padding: theme.spacing(2),
+  },
+}));
+
 function App() {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getCurrencies());
@@ -21,8 +30,8 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <AppBar />
+      <AppBar />
+      <div className={classes.container}>
         <Switch>
           <Route exact path="/">
             <CurrencyConverter />
--- src/pages/CurrencyConverter/index.tsx
+++ src/pages/CurrencyConverter/index.tsx
@@ -1,8 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import SwapVert from '@material-ui/icons/SwapVert';
+
+import { IconButton } from '@material-ui/core';
+import { StoreState } from '../../helpers/types';
+import getFormattedRate from '../../helpers/getFormattedRate';
+import CurrencyInput from '../../components/CurrencyInput';
+
+export interface CurrencyConverterState {
+  [key: string]: string | unknown
+}
 
 function CurrencyConverter() {
+  const { currencies, baseCurrency, ids } = useSelector(({ currencies: currState }: StoreState) => currState);
+  const [isSale, setIsSale] = useState(false);
+  const [inputState, setInputState] = useState({ primary: '', secondary: '' } as CurrencyConverterState);
+  const [state, setState] = useState({ primary: baseCurrency as string, secondary: '' } as CurrencyConverterState);
+
+  const saleKey = isSale ? 'sale' : 'buy';
+  const getInvertedId = (stateObj: CurrencyConverterState, id: string): string => Object.keys(stateObj).filter((key) => key !== id)[0];
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsSale(event.target.checked);
+  };
+
+  const handleSwap = () => {
+    setState({ primary: state.secondary, secondary: state.primary });
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    const invertedId = getInvertedId(state, id);
+    const statePrimary = state[id] as string;
+    const stateSecondary = state[invertedId] as string;
+
+    setInputState({
+      [id]: value,
+      [invertedId]: Number(value) ? getFormattedRate(
+        Number(value) * (Number(currencies[saleKey][statePrimary]) / Number(currencies[saleKey][stateSecondary])),
+      ) : '',
+    });
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<{ name?: string | undefined, value: unknown }>) => {
+    const { name, value } = event.target;
+    const invertedName = getInvertedId(state, name as string);
+    if (state[invertedName] === value) {
+      setState({ primary: state.secondary, secondary: state.primary });
+    } else {
+      setState({ ...state, [name as any]: value });
+    }
+  };
+
+  useEffect(() => {
+    if (!Object.values(state).includes('')) {
+      const statePrimary = state.primary as string;
+      const stateSecondary = state.secondary as string;
+
+      setInputState({
+        ...inputState,
+        secondary: Number(inputState.primary) ? getFormattedRate(
+          Number(inputState.primary) * (Number(currencies[saleKey][statePrimary]) / Number(currencies[saleKey][stateSecondary])),
+        ) : '',
+      });
+    }
+  }, [state, isSale]);
+
+  useEffect(() => {
+    if (ids.length > 1) {
+      setState({ ...state, secondary: ids.filter((id) => id !== state.primary)[0] });
+    }
+  }, [ids]);
+
   return (
-    <div>CurrencyConverter</div>
+    <Grid container justify="center" spacing={2}>
+      <Grid item>
+        <FormControlLabel
+          control={(
+            <Switch
+              checked={isSale}
+              onChange={handleSwitchChange}
+              color="primary"
+            />
+          )}
+          label={isSale ? 'Sale' : 'Buy'}
+        />
+      </Grid>
+      <Grid container spacing={2} justify="center">
+        <CurrencyInput
+          keyId="primary"
+          inputState={inputState}
+          state={state}
+          handleInputChange={handleInputChange}
+          handleSelectChange={handleSelectChange}
+          ids={ids}
+        />
+        <Grid item>
+          <IconButton onClick={handleSwap}>
+            <SwapVert />
+          </IconButton>
+        </Grid>
+        <CurrencyInput
+          keyId="secondary"
+          inputState={inputState}
+          state={state}
+          handleInputChange={handleInputChange}
+          handleSelectChange={handleSelectChange}
+          ids={ids}
+        />
+      </Grid>
+    </Grid>
   );
 }
 
